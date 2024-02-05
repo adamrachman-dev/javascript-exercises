e495f7dcda9e3860b18ec8c02311f9e54265b265
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-02-05 09:42:46 +0700

    Complete 06 exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (isCentury(year)) {
+        if (year % 400 === 0) return true;
+        return false;
+    }
+    return isLeap(year);
 };
+
+function isCentury(year) {
+    if (year % 100 === 0) return true;
+    return false;
+}
+
+function isLeap(year) {
+    if (year % 4 === 0) return true;
+    return false;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
