d0312ef94d54c6a70287cfd431cd8e51c3a2e154
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-02-05 09:06:16 +0700

    Complete 04 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let arr = args[0];
+    for (let i = 1; i < args.length; i++) {
+        let index = arr.indexOf(args[i]);
+        if (index < 0) continue;
+        arr.splice(index, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
