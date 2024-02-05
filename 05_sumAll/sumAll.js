510115ee431fea0f31a408cbf120d0f118e4566b
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-02-05 09:06:30 +0700

    Complete 05 exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+   if (isError(a, b)) return "ERROR";
+   if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+   }
+   let sum = 0;
+   for (let i = a; i <= b; i++) {
+        sum += i;
+   }
+   return sum;
 };
+
+function isError(a, b) {
+    if ((a < 0 || b < 0) || (typeof a !== "number" || typeof b !== "number")) {
+        return true;
+   }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
