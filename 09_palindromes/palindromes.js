d370f6e56be4bf7ffe9d03119c5c3b3d6e4e5cd2
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-02-06 13:44:43 +0700

    Complete 09 exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+   let arr = string.toLowerCase().split("");
+   let cleanArr = [];
+   let regex = /[a-z0-9]/;
+   for (const i of arr) {
+    if (regex.test(i)) {
+        cleanArr.push(i);
+    }
+   }
+   return cleanArr.join("") === cleanArr.reverse().join("");
 };
 
 // Do not edit below this line
