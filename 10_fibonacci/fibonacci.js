a2fa18048975251b3d6ae091d5ddd2a78b84e451
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-02-06 14:56:49 +0700

    Complete 10 exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    const phi = 1.618034;
+    return Math.round((phi ** number - (1 - phi) ** number) / Math.sqrt(5));
 };
 
 // Do not edit below this line
