df3327b4c074c24c4ef498ac1fb97f29625554db
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-02-06 09:10:39 +0700

    Complete 08 exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (!arr.length) return 0;
+  let sum = 0;
+  for (const i of arr) {
+    sum += i;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for (const i of arr) {
+    result *= i;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
