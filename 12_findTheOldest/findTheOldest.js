9bdacefdbd7ee3eaee9d04822dee2aa0d4310550
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-02-07 15:27:21 +0700

    Complete 12 exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+   let age = [];
+   for (const i of arr) {
+    if (i.yearOfDeath === undefined) i.yearOfDeath = new Date().getFullYear();
+    age.push(i.yearOfDeath - i.yearOfBirth);
+   }
+   const oldest = Math.max(...age);
+   return arr[age.indexOf(oldest)]; 
 };
 
 // Do not edit below this line
