f273242f59a7c55f3047c911ce0b371c37ca7cd2
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-03-12 14:23:27 +0700

    Edit calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,3 +1,33 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(bigNum, smallNum) {
+  return bigNum - smallNum;
+}
+
+function sum(arr) {
+  return arr.reduce((total, item) => total + item, 0);
+}
+
+function multiply(arr) {
+  return arr.reduce((total, item) => total * item);
+}
+
+function power(num, powerByValue) {
+  return num ** powerByValue;
+}
+
+function factorial(num) {
+  if (num === 0) return 1;
+  let result = 1;
+  for (let i = num; i > 1; i--) {
+    result *= i;
+  }
+  return result;
+}
+
+/* // legacy
 const add = function(num1, num2) {
   return num1 + num2;
 };
@@ -35,6 +65,7 @@ const factorial = function(num) {
   }
   return result;
 };
+*/
 
 // Do not edit below this line
 module.exports = {
