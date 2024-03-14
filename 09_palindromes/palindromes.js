3bd668823a58c500f614d98ae7002219032ee1c8
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-03-14 13:34:14 +0700

    Edit palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,15 @@
-const palindromes = function (string) {
-   let arr = string.toLowerCase().split("");
-   let cleanArr = [];
-   let regex = /[a-z0-9]/;
-   for (const i of arr) {
-    if (regex.test(i)) {
-        cleanArr.push(i);
-    }
-   }
-   return cleanArr.join("") === cleanArr.reverse().join("");
-};
+function palindromes(str) {
+    const normal = str.toLowerCase()
+    .split("")
+    .filter(char => /[a-z0-9]/.test(char))
+    .join("");
+
+    const reversed = normal.split("")
+    .reverse()
+    .join("");
+
+    return normal === reversed;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
