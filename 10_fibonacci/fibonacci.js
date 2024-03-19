2571b20755586c5c5410c71c91c5a5d70297d2d8
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-03-19 13:55:00 +0700

    Edit fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,8 +1,19 @@
-const fibonacci = function(number) {
+function fibonacci(num) {
+    const number = parseInt(num);
+
     if (number < 0) return "OOPS";
-    const phi = 1.618034;
-    return Math.round((phi ** number - (1 - phi) ** number) / Math.sqrt(5));
-};
+    if (number === 0) return 0;
+
+    let indexMin2 = 0;
+    let indexMin1 = 1;
+
+    for (let i = 2; i <= number; i++) {
+        let index = indexMin2 + indexMin1;
+        indexMin2 = indexMin1;
+        indexMin1 = index;
+    }
+    return indexMin1;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
