64c1c5efa66af314bc18085e4600083ea697f6d4
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-03-21 13:19:54 +0700

    Edit findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,12 +1,13 @@
 const findTheOldest = function(arr) {
-   let age = [];
-   for (const i of arr) {
-    if (i.yearOfDeath === undefined) i.yearOfDeath = new Date().getFullYear();
-    age.push(i.yearOfDeath - i.yearOfBirth);
-   }
-   const oldest = Math.max(...age);
-   return arr[age.indexOf(oldest)]; 
-};
+   const age = arr.map(key => {
+      if (key.yearOfDeath === undefined) key.yearOfDeath = new Date().getFullYear();
+      return key.yearOfDeath - key.yearOfBirth
+   });
+   
+   const oldest = age.slice().sort((a, b) => b - a);
+   const index = age.indexOf(oldest[0]);
+   return arr[index];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
